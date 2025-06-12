fb0d59c157a78e0d6dc27456cc03cf4b8d962e8c
Vanessa9541 <tvanessa41@yahoo.com>
2025-06-12 15:26:37 +0200

    changement de baseurl dans axiox

--- admin-frontend/src/api/axios.js
+++ admin-frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // Mets ici l'adresse de ton backend
+  baseURL: 'https://happyhour-backend.onrender.com', // Mets ici l'adresse de ton backend
   headers: {
     'Content-Type': 'application/json',
   },
