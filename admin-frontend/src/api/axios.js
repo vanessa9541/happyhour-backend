22404ce65f604fe97b6f9037743efa3a528f5e42
Vanessa9541 <tvanessa41@yahoo.com>
2025-06-12 15:30:45 +0200

    ajout de api

--- admin-frontend/src/api/axios.js
+++ admin-frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://happyhour-backend.onrender.com', // Mets ici l'adresse de ton backend
+  baseURL: 'https://happyhour-backend.onrender.com/api', // Mets ici l'adresse de ton backend
   headers: {
     'Content-Type': 'application/json',
   },
